f3270accd4147a03c88ba55fd8be33042c5f52df
Graf <gregory.yakovlev@gmail.com>
2021-08-06 11:47:49 +0300

    Добавлени интерфейс Disposable

---
+++ ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace NDict.ViewModels
+{
+    internal abstract class BaseViewModel : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public void OnPropertyChanged([CallerMemberName] string prop="")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName ="" )
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        private bool Disposed;
+        protected virtual void Dispose(bool _disposing)
+        {
+            if (!_disposing || Disposed) return;
+            Disposed = true;
+        }
+
+    }
+}
