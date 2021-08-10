daafc7738efa3100341c08609118e7835c79f732
Graf <65903163+GrafDev@users.noreply.github.com>
2021-08-10 18:04:23 +0300

    -

---
+++ Infrastructure/Commands/PlayApplicationCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NDict.Models;
+using NDict.Infrastructure.Commands.Base;
+using NDict.Infrastructure;
+using System.Windows;
+
+namespace NDict.Infrastructure.Commands
+{
+    class PlayApplicationCommand: Command
+    {
+        public override bool CanExecute(object parameter) => true;
+        public override void Execute(object parameter) => PlayModel();
+    }
+}
